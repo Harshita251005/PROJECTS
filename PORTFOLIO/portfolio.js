c5f133a4c6c95dced733134ea3b787e0c4c99fd1
Harshita <parasgoyal299@gmail.com>
2025-12-13 15:33:33 +0530

    done

--- PORTFOLIO/portfolio.js
+++ PORTFOLIO/portfolio.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings:[" Java Programmer ","Aspiring Software Engineer "," Problem Solver "],
+    strings:[" Full Stack Developer (MERN Stack) ","Backend Developer (Node.js / Java) "," Software Engineer (AI-Integrated Web Applications) "],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
